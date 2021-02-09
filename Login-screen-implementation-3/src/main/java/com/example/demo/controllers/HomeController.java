ea8729b8fde232be01caa680f5ad80a2516c8264
LAPTOP-CTBOG2G0\古郡　卓弥 <takuya199662@gmail.com>
2021-02-09 18:53:36 +0900

    内々定　ログイン画面実装

---
+++ Login-screen-implementation-3/src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/menuDetails")
+    public String getDetails(Model model) {
+        // menuDetails.htmlに画面遷移
+        return "menuDetails";
+    }
+	@GetMapping("/menuEdit")
+    public String getEdit(Model model) {
+        // menuEdit.htmlに画面遷移
+        return "menuEdit";
+    }
+	@GetMapping("/menuConfirmation")
+	public String getConfiramtion(Model model) {
+        // menuConfirmation.htmlに画面遷移
+        return "menuConfirmation";
+    }
+	@GetMapping("/menuDone")
+	public String getdone(Model model) {
+        // menuDone.htmlに画面遷移
+        return "menuDone";
+    }
+
+}

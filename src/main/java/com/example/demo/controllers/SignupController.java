64cddd4a3f3866d52f536a4b6a6a6ba3d47ec22c
LAPTOP-CTBOG2G0\古郡　卓弥 <takuya199662@gmail.com>
2021-02-16 00:13:11 +0900

    最新の内容で変更

--- src/main/java/com/example/demo/controllers/SignupController.java
+++ src/main/java/com/example/demo/controllers/SignupController.java
@@ -11,5 +11,15 @@ public class SignupController {
         // signup.htmlに画面遷移
         return "signup";
     }
+	@GetMapping("/signupConfirmation")
+	public String getSignupSuccess(Model model) {
+        // signupConfirmation.htmlに画面遷移
+        return "signupConfirmation";
+    }
+	@GetMapping("/signupDone")
+	public String getSignupDone(Model model) {
+        // signupDone.htmlに画面遷移
+        return "signupDone";
+    }
 
 }

29dd5ad12f6e93ceb7ed4abae030684edc85a502
KUJIRA <kujira.no.hitahito+github@gmail.com>
2021-03-01 07:13:29 +0900

    wip

--- src/main/java/com/example/demo/controllers/SignupController.java
+++ src/main/java/com/example/demo/controllers/SignupController.java
@@ -6,6 +6,10 @@ import com.example.demo.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,11 +24,14 @@ public class SignupController {
 
     private final UserService userService;
 
+    private final AuthenticationManager authenticationManager;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public SignupController(UserService userService, SignupFormValidator signupFormValidator) {
+    public SignupController(UserService userService, AuthenticationManager authenticationManager, SignupFormValidator signupFormValidator) {
         this.signupFormValidator = signupFormValidator;
+        this.authenticationManager = authenticationManager;
         this.userService = userService;
     }
 
@@ -47,6 +54,11 @@ public class SignupController {
         }
 
         userService.register(signupForm.getUserName(), signupForm.getPassword());
+
+        // TODO: ログイン
+//        Authentication authToken = new UsernamePasswordAuthenticationToken(signupForm.getUserName(), signupForm.getPassword());
+//        Authentication authResult = authenticationManager.authenticate(authToken);
+//        SecurityContextHolder.getContext().setAuthentication(authResult);
 
         logger.info("end   -- do signup.");
         return "signupDone";
